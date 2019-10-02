667ef2fa98d51191baebff8522ce76d75444fcad
Marco Antonio Chi Castillo <chimarcoantonio@gmail.com>
2019-10-01 21:21:28 -0500

    Modificacion

---
+++ Proyecto.js
@@ -0,0 +1,24 @@
+let stringText; //variable global
+function leerArchivo(e) {
+    var archivo = e.target.files[0];
+    if (!archivo) {
+      return;
+    }
+    var lector = new FileReader();
+    lector.onload = function(e) {
+      var contenido = e.target.result;
+      mostrarContenido(contenido);
+      stringText = contenido;
+  
+    }
+    lector.readAsText(archivo);
+  }
+  
+  function mostrarContenido(contenido) {
+    var elemento = document.getElementById('contenido-archivo');
+    elemento.innerHTML = contenido;
+  }
+  
+  document.getElementById('file-input').addEventListener('change', leerArchivo, false);
+  
+  //Trabajar con la variable stringText contiene todo el archivo.java
