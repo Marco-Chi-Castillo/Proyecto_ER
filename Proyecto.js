b4f95a40d3dd3a8ff0c7ba7653cb9c68f6b29f8b
Marco Antonio Chi Castillo <chimarcoantonio@gmail.com>
2019-10-02 19:57:47 -0500

    Estructuracion

--- Proyecto.js
+++ Proyecto.js
@@ -1,86 +1,84 @@
 let stringText; //variable global
 
 function leerArchivo(e) {
-    var archivo = e.target.files[0];
-    if (!archivo) {
-      return;
-    }
-    var lector = new FileReader();
-    lector.onload = function(e) {
-      var contenido = e.target.result;
-      mostrarContenido(contenido);
-      stringText = contenido;
-    }
-    lector.readAsText(archivo);
+  var archivo = e.target.files[0];
+  if (!archivo) {
+    return;
   }
-  
-  function mostrarContenido(contenido) {
-    var elemento = document.getElementById('contenido-archivo');
-    elemento.innerHTML = contenido;
+  var lector = new FileReader();
+  lector.onload = function (e) {
+    var contenido = e.target.result;
+    mostrarContenido(contenido);
+    stringText = contenido;
   }
-  
-  document.getElementById('file-input').addEventListener('change', leerArchivo, false);
+  lector.readAsText(archivo);
+}
 
-  //Trabajar con la variable stringText contiene todo el archivo.java
+function mostrarContenido(contenido) {
+  var elemento = document.getElementById('contenido-archivo');
+  elemento.innerHTML = contenido;
+}
 
-  //Buscar variables
-function variablesMostrar(){
-    let txt_imprimir = "";
-    let variables = new Array();
-    variables = stringText.match(/[\w]+( |)=( |)[\w]+( |)[+-\/\*]( |)[\w]+( |);/g);
-   for(let i =0; i < variables.length; i++){
-        txt_imprimir = txt_imprimir + `${i+1} = ${String(variables[i])} <br/>`; 
-    }
-    var elementoMat = document.getElementById('expresiones-mat');
-    elementoMat.innerHTML = txt_imprimir;
+document.getElementById('file-input').addEventListener('change', leerArchivo, false);
+
+//Trabajar con la variable stringText contiene todo el archivo.java
+//Buscar variables
+function variablesMostrar() {
+  let txt_imprimir = "";
+  let variables = new Array();
+  variables = stringText.match(/[\w]+( |)=( |)[\w]+( |)[+-\/\*]( |)[\w]+( |);/g);
+  for (let i = 0; i < variables.length; i++) {
+    txt_imprimir = txt_imprimir + `${i + 1} = ${String(variables[i])} <br/>`;
   }
-  
-  //let expresionBoleanos = /[\w]+( |)[=<>!|]+( |)[\w]+;/;
-  function booleanosMostrar(){
-    let txt_imprimir = "";
-    let booleanos = new Array();
-    //expresion regular que detecta expresiones booleanas de declaracion
-    //y dentro de funciones como if, while, for
-    //(|\()[\w.]+( |)(\={2}|\<[=]|\>[=]|\![=]|\|)( |)[\w]+(;|\))
-    booleanos = stringText.match(/(|\()[\w.]+( |)(\={2}|\<[=]|\>[=]|\![=]|\|)( |)[\w]+(;|\))/g);
-   for(let i =0; i < booleanos.length; i++){
-        txt_imprimir = txt_imprimir + `${i+1} = ${String(booleanos[i])} <br/>`; 
-    }
-    var elementoMat = document.getElementById('expresiones-boo');
-    elementoMat.innerHTML = txt_imprimir;
+  var elementoMat = document.getElementById('expresiones-mat');
+  elementoMat.innerHTML = txt_imprimir;
+}
+
+//let expresionBoleanos = /[\w]+( |)[=<>!|]+( |)[\w]+;/;
+function booleanosMostrar() {
+  let txt_imprimir = "";
+  let booleanos = new Array();
+  //expresion regular que detecta expresiones booleanas de declaracion
+  //y dentro de funciones como if, while, for
+  booleanos = stringText.match(/(|\()[\w.]+( |)(\={2}|\<[=]|\>[=]|\![=]|\|)( |)[\w]+(;|\))/g);
+  for (let i = 0; i < booleanos.length; i++) {
+    txt_imprimir = txt_imprimir + `${i + 1} = ${String(booleanos[i])} <br/>`;
   }
+  var elementoMat = document.getElementById('expresiones-boo');
+  elementoMat.innerHTML = txt_imprimir;
+}
 
 //Encriptacion
-function encriptarMostrar(){
+function encriptarMostrar() {
   let txt_imprimir = "";
   let texto = stringText;
   let en1, en2, en3, en4, en5;
   var palabrasEn = new Array();
-  let  comillas = texto.match(/(".*?"|'.*?')/g);
-  for(let i = 0; i< comillas.length;i++){
-       en1= comillas[i].replace(/(a)/gim,"1");
-       en2= en1.replace(/(e)/gim,"2");
-       en3= en2.replace(/(i)/gim,"3");
-       en4= en3.replace(/(o)/gim,"4");
-       en5= en4.replace(/(u)/gim,"5");
-       en6= en5.replace(/(l)/gim,"szs");
-       en7= en6.replace(/(n)/gim,"Q==");
-       en8= en7.replace(/(s)/gim,"ZzaA");
-       palabrasEn[i] = en8;
+  let comillas = texto.match(/(".*?"|'.*?')/g);
+  for (let i = 0; i < comillas.length; i++) {
+    en1 = comillas[i].replace(/(a)/gim, "1");
+    en2 = en1.replace(/(e)/gim, "2");
+    en3 = en2.replace(/(i)/gim, "3");
+    en4 = en3.replace(/(o)/gim, "4");
+    en5 = en4.replace(/(u)/gim, "5");
+    en6 = en5.replace(/(l)/gim, "szs");
+    en7 = en6.replace(/(n)/gim, "Q==");
+    en8 = en7.replace(/(s)/gim, "ZzaA");
+    palabrasEn[i] = en8;
   }
-  
-  for(let i = 0; i< palabrasEn.length;i++){
-     txt_imprimir = txt_imprimir + `${i+1} = ${String(palabrasEn[i])} <br/>`;
- }
- 
- var elementoMat = document.getElementById('expresiones-enc');
- elementoMat.innerHTML = txt_imprimir;
+
+  for (let i = 0; i < palabrasEn.length; i++) {
+    txt_imprimir = txt_imprimir + `${i + 1} = ${String(palabrasEn[i])} <br/>`;
+  }
+
+  var elementoMat = document.getElementById('expresiones-enc');
+  elementoMat.innerHTML = txt_imprimir;
 }
 //Eliminar comentarios del codigo
-function eliminarComentarios(){
+function eliminarComentarios() {
 
-  let eliminar = stringText.replace( /\/[\*|][\S\s]+?[\*]\/|\/[\/].*/g, "");
+  let eliminar = stringText.replace(/\/[\*|][\S\s]+?[\*]\/|\/[\/].*/g, "");
   var elementoMat = document.getElementById('contenido-archivo');
   elementoMat.innerHTML = eliminar;
-  
-  }
+
+}
